e7ab2972e8af784ea4a2761f7ca7a9ee7bb0c061
Kesigan Mukuntharathan <kesigan2308@gmail.com>
2025-03-19 18:19:26 +0530

    Initial commit

--- app/src/main/java/com/example/lab_exam2/summary.kt
+++ app/src/main/java/com/example/lab_exam2/summary.kt
@@ -3,12 +3,23 @@ package com.example.lab_exam2
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class summary : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_summary)
+
+
+
+        val back: ImageView = findViewById(R.id.leftArrow)
+
+        back.setOnClickListener {
+            val intent = Intent(this, exercise1::class.java)
+            startActivity(intent)
+            finish()
+        }
 
         val save: Button = findViewById(R.id.save)
 
